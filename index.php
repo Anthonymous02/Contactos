522d776d9e449073ace7b0faaa53dbfc1b502fde
26LUISEDUARDO <78490551+26LUISEDUARDO@users.noreply.github.com>
2021-02-10 16:28:50 -0400

    Tratando de hayar el fallo con git

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!doctype html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport"
@@ -7,9 +8,54 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Contacto</title>
     <link rel="stylesheet" href="assets/css/style.css">
+
 </head>
+
 <body>
 
-<h2> Javier sensei</h2>
+<header>
+    <!--navegacion-->
+        <nav class="nav" align="right">
+
+            <ul class="list">
+                <li class="espacio" align="right">
+                    <button> <a href="#">inicio</a></button>
+                </li>
+                <li>
+                    <button>  <a href="#">servicios</a></button>
+                </li>
+                <li>
+                    <button>  <a href="#">sobre nosotros</a></button>
+                </li>
+                <li>
+                    <button> <a href="#">Contactos</a></button>
+                </li>
+            </ul>
+        </nav>
+
+</header>
+
+        <!--1seccion-->
+        <section class="parrafo">
+
+            <h2 align="center">hostpizza</h2>
+            <p>
+                Biennvenidos a Hostpizza, la pizeria con el servicio mas rapido del pais,<br>
+                Hostpizza, tu pizza rapida y calientica...<br>
+            </p>
+
+            <div class="container">
+                <img src="assets/img/depositphotos_2330630-stock-photo-happy-attractive-cook-with-a.jpg">
+                    <br>
+                    <br>
+
+                      <p> <img src="assets/img/Pizza-con-niños-para-la-cuarentena.jpg"width="200px"height="200px">
+                          En Hostpizza  tratamos de crear un ambiente alegre y muy divertido nuestro personal siempre esta en<br>
+                          en continua interacion pendiente a las ordenes de cada uno de los clientes,habra un mesero por cada<br>
+                          mesa.
+                      </p>
+            </div>
+
+
 </body>
 </html>
